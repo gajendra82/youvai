256c04d517b405c742ca944795b7c15bc1215a83
aniket9909 <tekawadeaniket9909@gmail.com>
2025-07-30 10:34:34 +0530

    work on chnage in

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2174349095"
+    serviceWorkerVersion: "1435737866"
   }
 });

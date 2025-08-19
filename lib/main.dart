b13c5dee4668967033d3ad7dfc333b7f44425c72
aniket9909 <tekawadeaniket9909@gmail.com>
2025-08-19 18:29:58 +0530

    work on google login

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
     await Firebase.initializeApp(
-      options: FirebaseOptions(
+      options: const FirebaseOptions(
         apiKey: "AIzaSyAlg92sDvJb8xmuMt8yA9MtjbWrHMWV1oY",
         authDomain: "youvai-56995.firebaseapp.com",
-        projectId: "project-377693730311",
-        storageBucket: "youvai-56995.appspot.app",
+        // projectId: "project-377693730311",
+        projectId: "youvai-56995",
+        storageBucket: "youvai-56995.appspot.com",
         messagingSenderId: "377693730311",
         appId: "1:377693730311:web:24dfc047db461c18c3dca2",
       ),
